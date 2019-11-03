fde3c33fc27e742a95ef3a8d756b8db86796a96d
Vinod Kumar <vinodkumar@zeta.tech>
2019-11-03 20:05:39 +0530

    fix typo in image tag

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # build images
-docker build -t kumar003vinod/multi-client:latest -t kumar003vinod:multi-client:$GIT_SHA -f ./client/Dockerfile ./client/
-docker build -t kumar003vinod/multi-server:latest -t kumar003vinod:multi-server:$GIT_SHA -f ./server/Dockerfile ./server/
-docker build -t kumar003vinod/multi-worker:latest -t kumar003vinod:multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker/
+docker build -t kumar003vinod/multi-client:latest -t kumar003vinod/multi-client:$GIT_SHA -f ./client/Dockerfile ./client/
+docker build -t kumar003vinod/multi-server:latest -t kumar003vinod/multi-server:$GIT_SHA -f ./server/Dockerfile ./server/
+docker build -t kumar003vinod/multi-worker:latest -t kumar003vinod/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker/
 # push images
 docker push kumar003vinod/multi-client:latest
 docker push kumar003vinod/multi-server:latest
